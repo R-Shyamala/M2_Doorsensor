f12bc81078a7c34e3b4d3209636c5d7e2a0fbef6
R-Shyamala <101352498+R-Shyamala@users.noreply.github.com>
2022-04-20 12:43:39 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Doorsensor.h
@@ -0,0 +1,28 @@
+/**
+ * @file Doorsensor.h
+ * @author Shyamala.R (shyamala.18ece@sonatech.ac.in)
+ * @brief Finding door is open or close
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <avr/io.h>
+int main(void)
+/**
+ * @brief indicate the door is close or open
+   @return indication is done through LED		
+*/
+{
+  DDRB=DDRB&0b11111101;//fd
+  DDRC=DDRC|0b01000000;//40
+  while(1)
+  {
+if(PINB & 0b00000010)//02
+  PORTC=PORTC|0b01000000;//40
+    else
+    PORTC=PORTC&0b10111111;//bf
+  }
+  return 0;
+  }
